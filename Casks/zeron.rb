6b463354f45a78d80add4ace27d3efa98ce84359
Arvind Chembarpu <achembarpu@gmail.com>
2026-09-24 11:52:46 +0000

    zeron 0.2.86

--- Casks/zeron.rb
+++ Casks/zeron.rb
@@ -1,6 +1,6 @@
 cask "zeron" do
-  version "0.2.79"
-  sha256 "4b1742de20039b81adc5329d856c51d052776eb4fa9594531662b45f276dc5ca"
+  version "0.2.86"
+  sha256 "cfabc7367ec95fb9ebec935ecc151ec440f477010edf0d7d1957496c197f6459"
 
   url "https://github.com/zeronsh/zeron/releases/download/v#{version}/zeron-#{version}-macos-arm64.dmg"
   name "Zeron"
